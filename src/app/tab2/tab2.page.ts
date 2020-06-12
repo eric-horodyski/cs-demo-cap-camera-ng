149333fd3b56e60d3125f87463cf5a160c46105e
Eric Horodyski <horodyski@ionic.io>
2020-06-12 09:45:35 -0400

    Initial Commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { Plugins, CameraResultType, CameraSource } from "@capacitor/core";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
+  photo: SafeResourceUrl;
 
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer) {}
 
+  async takePicture() {
+    const image = await Plugins.Camera.getPhoto({
+      quality: 100,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+    });
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(
+      image && image.dataUrl
+    );
+  }
 }
